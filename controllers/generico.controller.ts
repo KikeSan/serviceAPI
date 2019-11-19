7f869ab1847fa5229b79b8006909b74f66ebee73
Kike Sanchez <kikin1601@hotmail.com>
2019-11-18 22:08:49 -0500

    Todo ok con Angular

--- controllers/generico.controller.ts
+++ controllers/generico.controller.ts
@@ -1,40 +1,3 @@
-/* let tareas = [
-  {
-    id: '2c5ea4c0-4067-11e9-8bad-9b1deb4d3b7d',
-    title: 'Tarea1',
-    description: 'loremp impsum',
-    status: 'todo',
-    fecha: 'Fri Jun 14 2019 17:06:03 GMT-0500 (hora estándar de Perú)'
-  },
-  {
-    id: '9125a8dc-52ee-365b-a5aa-81b0b3681cf6',
-    title: 'Tarea2',
-    description: 'loremp impsum',
-    status: 'todo',
-    fecha: 'Wed Jul 24 2019 17:06:03 GMT-0500 (hora estándar de Perú)'
-  },
-  {
-    id: 'c6235813-3ba4-3801-ae84-e0a6ebb7d138',
-    title: 'Cronograma',
-    description: 'loremp impsum',
-    status: 'todo',
-    fecha: 'Sat Aug 31 2019 17:06:03 GMT-0500 (hora estándar de Perú)'
-  },
-  {
-    id: 'e8b5a51d-11c8-3310-a6ab-367563f20686',
-    title: 'demo 4',
-    description: 'loremp impsum',
-    status: 'doing',
-    fecha: 'Mon Sep 2 2019 17:06:03 GMT-0500 (hora estándar de Perú)'
-  },
-  {
-    id: '1b9d6bcd-bbfd-4b2d-9b5d-ab8dfbbd4bed',
-    title: 'Tarea5',
-    description: 'loremp impsum',
-    status: 'complete',
-    fecha: 'Fri Sep 18 2019 17:06:03 GMT-0500 (hora estándar de Perú)'
-  }
-] */
 import httpStatus = require('http-status-codes');
 
 class GenericoController {
@@ -47,20 +10,12 @@ class GenericoController {
   }
 
   async listar(req, res) {
-    /* res.type("application/json").send({
-      name: "Fullstack",
-      ruta: req.url,
-      modelo: this.modelo
-    }) */
-    //console.log(tareas)
-    console.log('MODELO::::>', this.modelo);
     const results = await this.modelo.find();
     res.status(httpStatus.OK).json({
       status: httpStatus.OK,
       message: 'List',
       results
     });
-    //res.send(tareas)
   }
 
   async obtenerUno(req, res) {
@@ -72,18 +27,6 @@ class GenericoController {
       message: 'Document',
       result
     });
-
-    /* res.type('application/json').send({
-      name: 'Fullstack Detalle',
-      ruta: req.url
-    }) */
-    /* let result
-    tareas.forEach((element, index) => {
-      if (element.id == req.params.id) {
-        result = tareas[index]
-      }
-    })
-    res.send(result) */
   }
 
   async insertar(req, res) {
@@ -96,18 +39,6 @@ class GenericoController {
       status: httpStatus.CREATED,
       message: 'Document added'
     });
-    /* res.status(201).json({
-      status: 201,
-      message: 'Usuario insertado'
-    }) */
-    /* tareas.push({
-      id: req.query.id,
-      title: req.query.title,
-      description: req.query.description,
-      status: req.query.status,
-      fecha: req.query.fecha
-    })
-    res.send(tareas) */
   }
 
   async actualizar(req, res) {
@@ -120,23 +51,6 @@ class GenericoController {
       status: httpStatus.CREATED,
       message: 'Document updated'
     });
-
-    /* res.status(201).json({
-      status: 201,
-      message: 'Usuario actualizado'
-    }) */
-    /* for (let i = 0; i < tareas.length; i++) {
-      if (tareas[i].id == req.query.id) {
-        tareas[i] = {
-          id: req.query.id,
-          title: req.query.title,
-          description: req.query.description,
-          status: req.query.status,
-          fecha: req.query.fecha
-        }
-      }
-    }
-    res.send(tareas) */
   }
 
   async eliminar(req, res) {
@@ -148,17 +62,6 @@ class GenericoController {
       status: httpStatus.CREATED,
       message: 'Document deleted'
     });
-
-    /* res.status(201).json({
-      status: 201,
-      message: 'Usuario eliminado'
-    }) */
-    /* for (var i = 0; i < tareas.length; i++) {
-      if (tareas[i].id === req.params.id) {
-        tareas.splice(i, 1)
-      }
-    }
-    res.send(tareas) */
   }
 }
 
--- server.ts
+++ server.ts
@@ -27,6 +27,20 @@ if (cluster.isMaster) {
     return new Promise((resolve, reject) => {
       httpServer = http.createServer(app);
 
+      app.use((req, res, next) => {
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header(
+          'Access-Control-Allow-Headers',
+          'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method'
+        );
+        res.header(
+          'Access-Control-Allow-Methods',
+          'GET, POST, OPTIONS, PUT, DELETE'
+        );
+        res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+        next();
+      });
+
       app.use(bodyParser.json());
       app.use(bodyParser.urlencoded({ extended: true }));
 
