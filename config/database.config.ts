fb71f9400560c56f069c8af192ad7c70d8bc8edb
Kike Sanchez <kikin1601@hotmail.com>
2020-01-21 17:20:03 -0500

    config

--- config/database.config.ts
+++ config/database.config.ts
@@ -1,6 +1,7 @@
-const yenv = require('yenv');
-const env = yenv();
+//aqui va la config general del servicio
+const yenv = require("yenv")
+const env = yenv()
 
-const connectString = `mongodb+srv://${env.DB.USER}:${env.DB.PWD}@${env.DB.HOST}/${env.DB.NAME}?retryWrites=true&w=majority`;
+const connectString = `mongodb+srv://${env.DB.USER}:${env.DB.PWD}@${env.DB.HOST}/${env.DB.NAME}?retryWrites=true&w=majority`
 
-export { connectString };
+export { connectString }
