3318bc64531ff8c89ca2cb15a7e21c2fb59252f4
Kike Sanchez <kikin1601@hotmail.com>
2020-01-21 17:12:14 -0500

    ajuste pre heroku

--- routes/tareas.route.ts
+++ routes/tareas.route.ts
@@ -1,16 +1,16 @@
-import express = require('express')
-import { ControllerTarea } from '../controllers'
+import express = require("express")
+import { ControllerTarea } from "../controllers"
 //import { validador, esquemasAlumnos } from "../schemasValidators"
 
 const Router = express.Router()
 const controller = new ControllerTarea()
 
-Router.get('/', controller.listar)
+Router.get("/", controller.listar)
 
 /* Router.get("/detalle/:id/:nivel", validador(esquemasAlumnos.getAlumnosDetalle), controller.obtenerUno) */
-Router.get('/detalle/:_id', controller.obtenerUno)
-Router.post('/', controller.insertar)
-Router.put('/:_id', controller.actualizar)
-Router.delete('/:_id', controller.eliminar)
+Router.get("/detalle/:_id", controller.obtenerUno)
+Router.post("/", controller.insertar)
+Router.put("/:_id", controller.actualizar)
+Router.delete("/:_id", controller.eliminar)
 
 export default Router
